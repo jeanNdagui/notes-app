c9af57b59dff5ded7eef83adb4a600dbf0f2cf51
jeanNdagui <ndaguijeanlouismelanie@gmail.com>
2026-07-26 13:50:24 +0100

    test 3

--- drizzle/0003_lean_psynapse.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "users" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-DROP TABLE "users" CASCADE;--> statement-breakpoint
-ALTER TABLE "notes" DROP CONSTRAINT "notes_user_id_users_id_fk";
---> statement-breakpoint
-ALTER TABLE "notes" DROP COLUMN "user_id";
